a19a16edfbbbd0aec3f875ee8459938eaa6c5324
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-05-16 09:11:57 +0700

    Initial commit

---
+++ SK-Lib-cstdio-fgetc-Bagian4__CPP/Source.cpp
@@ -0,0 +1,39 @@
+#pragma warning(disable:4996)
+
+#include <cstdio>
+#include <cstdlib>
+#include <conio.h>
+
+/*
+    Source by CPPReference (https://en.cppreference.com)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main() {
+    int is_ok = EXIT_FAILURE;
+    FILE* fp = std::fopen("/tmp/test.txt", "w+");
+
+    if (!fp) {
+        std::perror("File opening failed");
+        return is_ok;
+    }
+
+    int c; // note: int, not char, required to handle EOF
+    
+    while ((c = std::fgetc(fp)) != EOF) { // standard C I/O file reading loop
+        std::putchar(c);
+    }
+
+    if (std::ferror(fp)) {
+        std::puts("I/O error when reading");
+    } else if (std::feof(fp)) {
+        std::puts("End of file reached successfully");
+        is_ok = EXIT_SUCCESS;
+    }
+
+    std::fclose(fp);
+
+    _getch();
+    return is_ok;
+}
